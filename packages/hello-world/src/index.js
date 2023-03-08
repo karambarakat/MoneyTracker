187e2eab008f29f9e88a5d023e9a3932ff24cdc0
karam barakat <karam.barakat.99@gmail.com>
2023-03-07 18:08:42 -0800

    test

--- packages/hello-world/src/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('@google-cloud/functions-framework');
-
-functions.http('helloGET', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'update'}!`);
-});
